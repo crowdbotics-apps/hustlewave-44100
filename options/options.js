80ccf87708518680a03a0bf5f5e3a125d75439b6
Crowdbotics <team@crowdbotics.com>
2023-10-22 20:22:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hustlewave-44100",
+  url: "https://hustlewave-44100.botics.co",
+  api: "https://hustlewave-44100.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
